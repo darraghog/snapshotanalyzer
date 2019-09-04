cda0d00e4253bf7bbf06a4b158ae2603cffa836b
Darragh O'Grady <darragh.ogrady@gmail.com>
2019-09-04 01:38:14 +0000

    added click

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,10 +1,23 @@
 import boto3
+import click
+
 
 session = boto3.Session(profile_name='default')
 ec2 = session.resource('ec2')
-
-if __name__ == '__main__':
+    
+@click.command()
+def list_instances():
+    "List EC2 instances"
     for i in ec2.instances.all():
-        print(i)
+        print(','.join( (
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)
+            ))
         
 
+if __name__ == '__main__':
+    list_instances()
+    
